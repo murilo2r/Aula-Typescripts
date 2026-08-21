b3f90f8aa1b639dd7d42290046d8b99aca7a2c24
murilo2r <murilo.monia@gmail.com>
2026-08-20 21:03:38 -0300

    Inicializando.

---
+++ Aula1.py
@@ -0,0 +1,5 @@
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+
+print("Olá, " + nome + "! Você tem " + str(idade) + " anos.")
+print("Obrigado por participar da aula!")
